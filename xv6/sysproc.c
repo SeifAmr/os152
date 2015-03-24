8ac89c68640bd0c538575a74d18965b3151139a7
chen <chen@web-pick.com>
2015-03-24 23:08:52 +0200

    phase 1

--- xv6/sysproc.c
+++ xv6/sysproc.c
@@ -14,16 +14,16 @@ sys_fork(void)
 }
 
 int
-sys_exit(void)
+sys_exit(int status)
 {
-  exit();
+  exit(0);
   return 0;  // not reached
 }
 
 int
-sys_wait(void)
+sys_wait(int *status)
 {
-  return wait();
+  return wait(status);
 }
 
 int
